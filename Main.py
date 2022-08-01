5afd4f2b0918430a42f21ee0ac93350d1293807d
agnespaulurk21cs1020 <109952516+agnespaulurk21cs1020@users.noreply.github.com>
2022-08-01 15:26:01 +0530

    second commit

--- Main.py
+++ Main.py
@@ -119,7 +119,7 @@ solution = Solution(length_of_text)
 
 # push/enqueue all the characters of string text to stack
 for index in range(length_of_text):
-    self.stack.append(text[index])
+    Solution.push_character(text[index])
 
 is_palindrome = True
 '''
